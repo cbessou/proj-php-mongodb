93862d9f4184bc818a59d67b968f9c1e1216f0e3
cbessou <claire.bessou@laposte.net>
2017-01-23 13:53:05 +0100

    gestion session page d'accueil

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+if(isset($_POST['deco'])){
+    session_destroy();
+    session_start();
+}
+if(isset($_POST['util'])&&isset($_POST['mdp'])){
+    //interroger la base de données
+    //si corespondance la session recoit l'identifiant et le type
+    $_SESSION['util']=$_POST['util'];
+    
+    //sinon erreur identifiant ou mdp
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,6 +19,20 @@
     <title>Projet PHP - MongoDB</title>
 </head>
 <body>
+    <header>
+        <form name='login' action="" method="post">
+            <?php
+            if (isset($_SESSION['util'])){
+                echo '<input type="submit" name="deco" value="Déconnexion">';
+                echo '<a href="maintenance.php">Page de maintenance</a>';
+            }else{
+                echo '<input type="text" name="util" placeholder="Utilisateur">
+            <input type="password" name="mdp" placeholder="Mot de passe">
+            <input type="submit" value="Connexion">';
+            }
+            ?>
+        </form>
+    </header>
     <h1>Bienvenue sur notre Site.</h1>
     <h1>Geo France.</h1><br/>
     <h2>Géolocalisation par Recherche</h2>
