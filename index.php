75e8ade45f84bb20e4fd59ee31957bc24214d01f
Fournier <davidalexandre.fournier.pro@gmail.com>
2017-01-23 13:00:36 +0100

    creation formulaire

--- index.php
+++ index.php
@@ -5,7 +5,24 @@
     <title>Projet PHP - MongoDB</title>
 </head>
 <body>
-
+    <h1>Bienvenue sur notre Site.</h1>
+    <h1>Geo France.</h1><br/>
+    <h2>Géolocalisation par Recherche</h2>
+    <h2>en France Métropolitaine et Corse.</h2><br/>
+    <p>
+        
+    // création du formulaire html
+    <form action="" method="post">
+    <fieldset>
+        <legend>Villes</legend>
+        <label>Villes: <input type="text" name="villes">&nbsp;Champs Obligatoire.</label>
+        <label>Département: <input type="text" name="departement">&nbsp;Champs Facultatif.</label>
+        <label>Région: <input type="text" name="region">&nbsp;Champs Facultatif.</label>
+        </fieldset>
+        <input type="submit" value="Valider">
+        <input type="reset" value="Réinitialiser">
+    </form>
+    </p>
     
 </body>
 </html>
