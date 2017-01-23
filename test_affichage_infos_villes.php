12c299a2b3cb7d3f4a3803ff82a502f9e395c8d4
Ainsley Remy <ainsley.remy.pro@gmail.com>
2017-01-23 16:55:23 +0100

    Affichage de la ville mais condition à rajouter pour ville=NULL et regler pb de majusculles

--- test_affichage_infos_villes.php
+++ test_affichage_infos_villes.php
@@ -1,5 +1,4 @@
 <?php 
-
 try { 
 
 // les paramètres de connexion
@@ -8,14 +7,43 @@ $dsn='mongodb://localhost:27017';
 // création de l'instance de connexion
 $mgc = new MongoDB\Driver\Manager($dsn);
 
+}
+
+ catch(MongoDB\Driver\Exception $e) {
+  // en cas d'erreur on montre le message reçu.
+  die (sprintf("<h2>traitement de l'erreur survenue durant le traitement</h2>\n<pre>%s</pre>\n", $e->getMessage()));
+}
+
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Demo</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="Demo project">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <style type="text/css"></style>
+    </head>
+
+    <body>
+        <form action="" method="get">
+            <fieldset>Nom
+                <input type="text" name="nom" />
+            </fieldset>
+            <input type="submit" value="Envoyer">
+        </form>
+<?php 
+        $ville=$_GET['nom'];
+        
+        //à remplacer par al variable recherchée
+
+        $filter = ['nom' => $ville];
     
-//à remplacer par al variable recherchée
-$ville='Toulouse';
-$filter = ['nom' => $ville];
-    
-    
-// création de requête
-$query = new MongoDB\Driver\Query($filter);
+        // création de requête
+        $query = new MongoDB\Driver\Query($filter);
     
     
 // exécution de la requête par la connexion
@@ -28,14 +56,14 @@ $curs = $mgc->executeQuery(
 // parcours du curseur résultant
 foreach($curs as $doc) {
   echo $doc->nom, ' <br>pop: '.$doc->pop, '<br>cp: ', $doc->cp,'<br>lat: ', $doc->lat,'<br>lon: ',$doc->lon."\n";
-}  
+} 
+        
+        
+        
+        
+        
+        ?>
 
-    
-}
+    </body>
 
- catch(MongoDB\Driver\Exception $e) {
-  // en cas d'erreur on montre le message reçu.
-  die (sprintf("<h2>traitement de l'erreur survenue durant le traitement</h2>\n<pre>%s</pre>\n", $e->getMessage()));
-}
-
-?>
+    </html>
