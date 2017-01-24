0abbf0866f7118ede83034d10714b120841f225e
mickael.meunier <mickael.meunier.pro@gmail.com>
2017-01-24 12:53:34 +0100

    Ajout des champs éditeurs

--- maintenance.php
+++ maintenance.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
+/*session_start();
 if(!isset($_SESSION['util'])) {
     header("Location: /index.php",TRUE,303);
     echo 'erreur de connexion';
-}
+}*/
 ?>
     
 <!DOCTYPE html>
@@ -14,8 +14,29 @@ if(!isset($_SESSION['util'])) {
 </head>
 <body>
 
+<h1>Maintenance de la base de données</h1>
+    <h1>Geo France</h1><br/>
+    <h2>Vous êtes éditeur :</h2>
+    
+    <!--création du formulaire html-->
+    <p>
+    <form action="" method="">
+    <fieldset>
+        <legend>Mise à jour du code postal et de la population</legend>
+        <p>Valeurs actuelles</p>
+        <label>Code postal: <input type="text" name="cpnow"</label><br/>
+        <label>Population: <input type="text" name="popnow"></label><br/>
+        <p>Nouvelles valeurs</p>
+        <label>Code postal: <input type="text" name="cpnew"></label><br/>
+        <label>Population: <input type="text" name="popnew"></label><br/>
+    </fieldset>
+        <input type="submit" value="Valider">
+        <input type="reset" value="Ré-initialiser">
+    </form>
+    </p>
+
 <?php
-    echo 'Bonjour&nbsp;'.$_SESSION['util'].'&nbsp;et Bienvenue.'
+ //   echo 'Bonjour&nbsp;'.$_SESSION['util'].'&nbsp;et Bienvenue.'
 ?>
     
 </body>
