0e2a6c79a7b9b921bb2353ce35cf62fe81dd3af4
mickael.meunier <mickael.meunier.pro@gmail.com>
2017-01-26 12:24:03 +0100

    ajout de codes avec récupération infos villes

--- maintenance.php
+++ maintenance.php
@@ -1,11 +1,11 @@
 <?php
-/*session_start();
+session_start();
+//verif connexion et redirection
 if(!isset($_SESSION['util'])) {
     header("Location: /index.php",TRUE,303);
     echo 'erreur de connexion';
-}*/
+}
 ?>
-    
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,25 +17,65 @@ if(!isset($_SESSION['util'])) {
 <h1>Maintenance de la base de données</h1>
     <h1>Geo France</h1><br/>
     <h2>Vous êtes éditeur :</h2>
+
+<?php
+//on verifie qu'on a bien recu la ville a modifier
+if(isset($_GET['idv'])) {
+    echo'<pre>';
+    $idv = $_GET['idv'];
+    print_r($idv);
+    //on récupère les infos de la ville
+    try { 
+    // les paramètres de connexion
+        $dsn='mongodb://localhost:27017';
+    // création de l'instance de connexion
+        $mgc = new MongoDB\Driver\Manager($dsn);
+
+    // filtre sur l'id de la ville
+    // préparation de la requête 
+$filter = ['_id'=>$idv];
+print_r($filter);
+$options = ['projection' => ['nom' => 1, '_id_dept' => 1, 'pop' => 1, 'cp' => 1]];
+$query = new MongoDB\Driver\Query($filter, $options);
+print_r($query);
+    // lancement de la requête
+$curs = $mgc->executeQuery('geo_france.villes', $query);
+
+print_r($curs);  
+$res = $curs -> toArray(); 
+print_r($res);
+echo'</pre>';
+    }
+    catch(MongoDB\Driver\Exception $e) {
+        // en cas d'erreur on montre le message reçu.
+        die (sprintf("<h2>traitement de l'erreur survenue durant le traitement</h2>\n<pre>%s</pre>\n", $e->getMessage()));
+    }
+
     
-    <!--création du formulaire html-->
-    <p>
+
+    
+    echo'<!--création du formulaire html-->
     <form action="" method="">
     <fieldset>
         <legend>Mise à jour du code postal et de la population</legend>
-        <p>Valeurs actuelles</p>
-        <label>Code postal: <input type="text" name="cpnow"</label><br/>
-        <label>Population: <input type="text" name="popnow"></label><br/>
         <p>Nouvelles valeurs</p>
-        <label>Code postal: <input type="text" name="cpnew"></label><br/>
-        <label>Population: <input type="text" name="popnew"></label><br/>
-    </fieldset>
+        <label>Code postal: <input type="text" name="cpnew">Ancienne valeur</label><br/>
+        <label>Population: <input type="text" name="popnew">Ancienne valeur</label><br/>';
+
+    if($_SESSION['profil']=='admin') {
+        echo'<label>Nom de la région: <input type="text" name="regnew">Ancienne valeur</label><br/>';
+    }
+
+
+
+    echo'</fieldset>
         <input type="submit" value="Valider">
         <input type="reset" value="Ré-initialiser">
-    </form>
-    </p>
-
-<?php
+    </form>';
+} else {
+    echo '<p>Aucune ville sélectionnée, <a href="index.php">choisissez une ville</a></p>';
+}
+;
  //   echo 'Bonjour&nbsp;'.$_SESSION['util'].'&nbsp;et Bienvenue.'
 ?>
     
